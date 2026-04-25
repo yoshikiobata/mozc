e15b15e6ae2ff0fbd5a8b219fc554744f95de31a
yoshiki.obata <yoshiki.obata@gmail.com>
2026-04-25 18:45:39 +0900

    Revert "fix custom widget"
    This reverts commit f97bcf8d21ebfe5488c93aefbb5739f010a5ce59.

--- src/gui/config_dialog/config_dialog.h
+++ src/gui/config_dialog/config_dialog.h
@@ -75,6 +75,9 @@ class ConfigDialog : public QDialog, private Ui::ConfigDialog {
   virtual void SelectSuggestionSetting(int state);
   virtual void LaunchAdministrationDialog();
 
+ protected:
+  bool eventFilter(QObject *obj, QEvent *event) override;
+
  private:
   bool GetConfig(config::Config *config);
   bool SetConfig(const config::Config &config);
